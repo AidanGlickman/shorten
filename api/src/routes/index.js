af5be12b8f2a2a1f83eb9c08c89976e2b7efd7e7
Aidan Glickman <aidan@aidanglickman.com>
2020-08-17 16:28:32 -0400

    add link to route index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
 import admin from './admin';
 import workspace from './workspace';
 import user from './user';
+import link from './link';
 import auth from './auth';
 
 export default {
   admin,
   user,
+  link,
   workspace,
   auth,
 };
