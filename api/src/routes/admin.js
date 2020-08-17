9e61d2ba03e5f63eeb38d2689ac21ba1da5125fd
Aidan Glickman <aidan@aidanglickman.com>
2020-08-17 16:32:43 -0400

    add admin route to list users

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import authService from '../services/authService';
 import models from '../models';
+import userService from '../services/userService';
 
 const router = Router();
 
@@ -20,6 +21,22 @@ router.post('/impersonate', async (req, res) => {
     },
     jwt: authService.generateToken(userRecord),
   });
+});
+
+router.post('/user', async (req, res) => {
+  let users;
+  try {
+    users = await userService.getUsers(
+      req.body.page,
+      req.body.usersPer,
+      req.body.filters,
+      req.body.search
+    );
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+  console.log(users);
+  return res.send(users);
 });
 
 export default router;
