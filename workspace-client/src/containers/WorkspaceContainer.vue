4b0a67486404ff567b309098dd3bcc6792c63021
Aidan Glickman <aidan@aidanglickman.com>
2020-10-13 15:19:21 +0300

    refactor workspace-container

--- workspace-client/src/containers/WorkspaceContainer.vue
+++ workspace-client/src/containers/WorkspaceContainer.vue
@@ -1,15 +1,18 @@
 <template>
-<div>
-  <code>{{this.code}}.srn.pw</code>
-  <div v-if="this.workspace">
-    <h1>{{this.workspace.title}}</h1>
-    <h2>{{this.workspace.description}}</h2>
-    <LinksContainer v-if="this.fetched" :links="this.workspace.links"></LinksContainer>
+  <div>
+    <code>{{ this.code }}.srn.pw</code>
+    <div v-if="this.workspace">
+      <h1>{{ this.workspace.title }}</h1>
+      <h2>{{ this.workspace.description }}</h2>
+      <LinksContainer
+        v-if="this.fetched"
+        :links="this.workspace.links"
+      ></LinksContainer>
+    </div>
+    <div v-if="this.error">
+      {{ this.error }}
+    </div>
   </div>
-  <div v-if="this.error">
-      {{this.error}}
-  </div>
-</div>
 </template>
 
 <script>
@@ -31,18 +34,20 @@ export default {
     };
   },
   mounted() {
-    console.log(this.code);
-    this.$api.get(`workspace/${this.code}`).then((res) => {
-      this.workspace = res.data.workspace;
-      this.fetched = true;
-    }).catch((err) => {
-      console.log(err.response);
-      this.error = err.response.data;
-    });
+    document.title = `${this.code} on srn.pw`;
+    this.$api
+      .get(`workspace/${this.code}`)
+      .then((res) => {
+        this.workspace = res.data.workspace;
+        this.fetched = true;
+      })
+      .catch((err) => {
+        console.log(err.response);
+        this.error = err.response.data;
+      });
   },
 };
 </script>
 
 <style>
-
 </style>
