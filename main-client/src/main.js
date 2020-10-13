4564374bf3287b0f8478175941d4eb5fd8f092e4
Aidan Glickman <aidan@aidanglickman.com>
2020-10-13 15:28:45 +0300

    add bootstrap-vue

--- main-client/src/main.js
+++ main-client/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue';
+import api from '@/lib/api';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 Vue.config.productionTip = false;
+
+Vue.prototype.$api = api;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
