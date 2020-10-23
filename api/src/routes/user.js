9e21ecbb6489f68c64b2c404baf9cfc8ed043466
Aidan Glickman <aidan@aidanglickman.com>
2020-10-23 12:05:41 -0400

    add user ability to get workspace

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import userService from '../services/userService';
+import workspaceService from '../services/workspaceService';
+import models from '../models';
 
 const router = Router();
 
@@ -11,6 +13,25 @@ router.get('/workspaces', async (req, res) => {
   } catch (error) {
     return res.status(400).send(error.message);
   }
+});
+
+router.get('/workspaces/:code', async (req, res) => {
+  const workspace = await workspaceService.getWorkspace(req.params.code);
+  if (workspace == null) {
+    return res.status(404).send('no workspace matching that code');
+  }
+  if (workspace.userId !== req.currentUser.id) {
+    return res.status(401).send('User does not own this workspace.');
+  }
+  const links = await models.Link.findAll({
+    where: {
+      workspaceId: workspace.id,
+    },
+  });
+  return res.send({
+    workspace,
+    links,
+  });
 });
 
 router.delete('/', async (req, res) => {
