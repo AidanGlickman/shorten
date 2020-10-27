e6f7811a3c48e6719337bce947c3d33094a878ea
Aidan Glickman <aidan@aidanglickman.com>
2020-10-27 13:34:01 -0400

    get rid of redis password

--- api/src/helpers/redisClient.js
+++ api/src/helpers/redisClient.js
@@ -3,7 +3,6 @@ import Redis from 'ioredis';
 const redis = new Redis({
   port: process.env.REDIS_PORT,
   host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASS,
   db: process.env.REDIS_DB,
 });
 
