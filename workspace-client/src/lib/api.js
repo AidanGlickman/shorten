ef4c6e80f08a816b0bf06309dbd443f652de37b7
Aidan Glickman <aidan@aidanglickman.com>
2020-11-03 18:44:17 -0500

    fix workspace api connection for production

--- workspace-client/src/lib/api.js
+++ workspace-client/src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://${process.env.VUE_APP_BASE_URL}/api`,
+  baseURL: `http${proces.env.NODE_ENV === 'production' ? 's' : ''}://${process.env.VUE_APP_BASE_URL}/api`,
 });
