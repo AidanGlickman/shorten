9e65a1bd1d62b3c3de97418e3f9cccf695b53203
Aidan Glickman <aidan@aidanglickman.com>
2020-11-12 14:23:51 -0500

    redo cards

--- workspace-client/src/components/LinkCard.vue
+++ workspace-client/src/components/LinkCard.vue
@@ -1,25 +1,25 @@
 <template>
-  <b-col xs=12 md="6" lg="3" class="mb-3 text-center">
+  <b-col col="4" sm="4" md="3" lg="2" class="mb-3 text-center">
     <a :href="link.link">
-    <b-card header-tag="header" footer-tag="footer">
-      <template #header>
-        {{workspaceCode}}.srn.pw/{{link.code}}
-      </template>
+      <b-card header-tag="header" footer-tag="footer" :id="`${_uid}_card`" class="h-100">
+        <template #header>
+          {{link.name}}
+        </template>
 
-      <b-card-body>
-        <i :class="`lnir lnir-32 lnir-${link.icon}`"></i>
-        <br>
-        <span class="nowrap" :id="`${_uid}_linkText`">{{link.link}}</span>
-        <b-popover :target="`${_uid}_linkText`" triggers="hover" placement="bottom">
-          <template #title>Full Link</template>
-          {{link.link}}
-        </b-popover>
-      </b-card-body>
+    <b-popover :target="`${_uid}_card`" triggers="hover" placement="bottom">
+      <template #title><a :href="link.link">{{workspaceCode}}.srn.pw/{{link.code}}</a></template>
+            <a :href="link.link">{{link.link}}</a>
+    </b-popover>
 
-      <template #footer>
-        {{link.name}}
-      </template>
-    </b-card>
+        <b-card-body>
+          <i :class="`lnir lnir-32 lnir-${link.icon}`"></i>
+        </b-card-body>
+
+        <template #footer>
+          {{link.code}}
+        </template>
+      </b-card>
+
     </a>
   </b-col>
 </template>
